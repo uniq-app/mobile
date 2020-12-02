5c7952e6b2d56804e939f322a387150a55440123
maciejows <owsianny.maciej@gmail.com>
2020-12-02 22:42:04 +0100

    Initialized folder structure

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:uniq/main.dart';
+import 'package:uniq/src/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
