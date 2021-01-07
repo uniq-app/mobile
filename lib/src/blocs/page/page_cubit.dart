62d68a0e195dfbc855ee1dd783f3a8db1f79b3f3
maciejows <owsianny.maciej@gmail.com>
2021-01-07 20:10:02 +0100

    Go back button navigates to last visited page instead of closing app

--- lib/src/blocs/page/page_cubit.dart
+++ lib/src/blocs/page/page_cubit.dart
@@ -3,9 +3,18 @@ import 'package:bloc/bloc.dart';
 part 'page_state.dart';
 
 class PageCubit extends Cubit<PageState> {
+  List<PageState> pageStack = List.from([PageState.homePage]);
   PageCubit() : super(PageState.homePage);
 
   setPage(PageState state) {
+    if (!pageStack.contains(state)) pageStack.add(state);
     emit(state);
+  }
+
+  popOne() {
+    if (pageStack.length > 0) {
+      pageStack.removeLast();
+      emit(pageStack.last);
+    }
   }
 }
--- lib/src/screens/application_page.dart
+++ lib/src/screens/application_page.dart
@@ -1,19 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:uniq/src/blocs/auth/auth_bloc.dart';
-import 'package:uniq/src/blocs/board/board_bloc.dart';
 import 'package:uniq/src/blocs/page/page_cubit.dart';
-import 'package:uniq/src/blocs/photo/photo_bloc.dart';
-import 'package:uniq/src/blocs/picked_images/picked_images_cubit.dart';
-import 'package:uniq/src/blocs/select_board_dialog/select_board_cubit.dart';
 import 'package:uniq/src/screens/home_page.dart';
 import 'package:uniq/src/screens/image_library_page.dart';
 import 'package:uniq/src/screens/search_page.dart';
 import 'package:uniq/src/screens/take_photo_screen.dart';
 import 'package:uniq/src/screens/user_settings_page.dart';
-import 'package:uniq/src/services/board_api_provider.dart';
-import 'package:uniq/src/services/photo_api_provider.dart';
 import 'package:uniq/src/shared/components/bottom_navbar.dart';
+import 'package:move_to_background/move_to_background.dart';
 
 class ApplicationPage extends StatefulWidget {
   @override
@@ -26,24 +20,35 @@ class _ApplicationPageState extends State<ApplicationPage> {
     return BlocProvider<PageCubit>(
       create: (context) => PageCubit(),
       child: Builder(
-        builder: (context) => Scaffold(
-          body: BlocBuilder<PageCubit, PageState>(
-            builder: (BuildContext context, PageState state) {
-              if (state == PageState.homePage) {
+        builder: (context) => WillPopScope(
+          onWillPop: () async {
+            List<PageState> currentStack = context.read<PageCubit>().pageStack;
+            if (currentStack.last == PageState.homePage)
+              MoveToBackground.moveTaskToBack();
+            else
+              context.read<PageCubit>().popOne();
+
+            return false;
+          },
+          child: Scaffold(
+            body: BlocBuilder<PageCubit, PageState>(
+              builder: (BuildContext context, PageState state) {
+                if (state == PageState.homePage) {
+                  return HomePage();
+                } else if (state == PageState.searchPage) {
+                  return SearchPage();
+                } else if (state == PageState.cameraPage) {
+                  return TakePhotoScreen();
+                } else if (state == PageState.libraryPage) {
+                  return ImageLibraryPage();
+                } else if (state == PageState.profilePage) {
+                  return UserSettingsPage();
+                }
                 return HomePage();
-              } else if (state == PageState.searchPage) {
-                return SearchPage();
-              } else if (state == PageState.cameraPage) {
-                return TakePhotoScreen();
-              } else if (state == PageState.libraryPage) {
-                return ImageLibraryPage();
-              } else if (state == PageState.profilePage) {
-                return UserSettingsPage();
-              }
-              return HomePage();
-            },
+              },
+            ),
+            bottomNavigationBar: BottomNavbar(),
           ),
-          bottomNavigationBar: BottomNavbar(),
         ),
       ),
     );
