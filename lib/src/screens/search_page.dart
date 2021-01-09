27bb0c5a982805865cc99f057e623a02b2ca2eb1
Gracjan Trawiński <44528307+gratraw@users.noreply.github.com>
2021-01-09 17:12:39 +0100

    Merged with master and added changes

--- lib/src/screens/search_page.dart
+++ lib/src/screens/search_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:uniq/src/blocs/search_boards/search_boards_bloc.dart';
+import 'package:uniq/src/models/board.dart';
 import 'package:uniq/src/services/board_api_provider.dart';
 import 'package:uniq/src/shared/components/board_list.dart';
 import 'package:uniq/src/shared/components/custom_error.dart';
@@ -41,59 +42,90 @@ class _SearchPageState extends State<SearchPage>
   }
 
   @override
-  Widget build(BuildContext _) {
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return BlocProvider<SearchBoardsBloc>(
       create: (context) => SearchBoardsBloc(
         boardRepository: BoardApiProvider(),
       ),
-      child: SlideTransition(
-        position: _offsetAnimation,
-        child: Builder(
-          builder: (context) => Scaffold(
-            appBar: AppBar(
-              title: Text('Search'),
-            ),
-            body: Column(
-              children: [
-                UniqInputField(
-                  color: Theme.of(context).accentColor,
-                  isObscure: false,
-                  labelText: "Search",
-                  controller: queryController,
+      child: Builder(
+        builder: (context) => Scaffold(
+          body: CustomScrollView(
+            slivers: <Widget>[
+              SliverAppBar(
+                title: Column(
+                  children: [
+                    UniqInputField(
+                      color: Theme.of(context).accentColor,
+                      isObscure: false,
+                      labelText: "Search",
+                      controller: queryController,
+                    ),
+                    OutlinedButton(
+                      onPressed: () => {
+                        if (queryController.text.length > 0)
+                          context
+                              .read<SearchBoardsBloc>()
+                              .add(SearchForBoards(query: queryController.text))
+                      },
+                      child: Text('Search'),
+                    ),
+                  ],
                 ),
-                OutlinedButton(
-                  onPressed: () => {
-                    if (queryController.text.length > 0)
-                      context
-                          .read<SearchBoardsBloc>()
-                          .add(SearchForBoards(query: queryController.text))
-                  },
-                  child: Text('Search'),
+                pinned: true,
+                expandedHeight: 60.0,
+                flexibleSpace: FlexibleSpaceBar(
+                  background: Column(
+                    children: [],
+                  ),
                 ),
-                BlocBuilder<SearchBoardsBloc, SearchBoardsState>(
-                  builder: (BuildContext context, SearchBoardsState state) {
-                    if (state is SearchForBoardsLoading) {
-                      return Loading();
-                    } else if (state is SearchForBoardsSuccess) {
-                      return Text('Found');
-                      // TODO: Here
-                      //return BoardList(state.boardResults.results);
-                    } else if (state is SearchForBoardsNotFound) {
-                      return Text('Not found boards matching query');
-                    } else if (state is SearchForBoardsError) {
-                      return CustomError(
-                        message: state.error.message,
-                        onTap: () => {},
-                      );
-                    } else {
-                      return Center(
-                        child: Text("Search for boards"),
-                      );
-                    }
-                  },
-                ),
-              ],
-            ),
+              ),
+              BlocBuilder<SearchBoardsBloc, SearchBoardsState>(
+                builder: (BuildContext context, SearchBoardsState state) {
+                  if (state is SearchForBoardsLoading) {
+                    return SliverList(
+                      delegate: SliverChildListDelegate(
+                        [
+                          Loading(),
+                        ],
+                      ),
+                    );
+                  } else if (state is SearchForBoardsSuccess) {
+                    final List<Board> boards = state.boardResults.results;
+                    return BoardList(boards);
+                  } else if (state is SearchForBoardsNotFound) {
+                    return SliverList(
+                      delegate: SliverChildListDelegate(
+                        [
+                          Text('Not found boards matching query'),
+                        ],
+                      ),
+                    );
+                  } else if (state is SearchForBoardsError) {
+                    return SliverList(
+                      delegate: SliverChildListDelegate(
+                        [
+                          CustomError(
+                            message: state.error.message,
+                            onTap: () => {},
+                          )
+                        ],
+                      ),
+                    );
+                  } else {
+                    return SliverList(
+                      delegate: SliverChildListDelegate(
+                        [
+                          Center(
+                            child: Text("Search for boards"),
+                          ),
+                        ],
+                      ),
+                    );
+                  }
+                },
+              ),
+            ],
           ),
         ),
       ),
--- lib/src/shared/components/board_list.dart
+++ lib/src/shared/components/board_list.dart
@@ -10,66 +10,25 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 class BoardList extends StatelessWidget {
   final List<Board> boards;
   BoardList(this.boards);
-  final String userName = "Zdzisław";
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-    return CustomScrollView(
-      slivers: <Widget>[
-        SliverAppBar(
-            automaticallyImplyLeading: false,
-            pinned: false,
-            floating: true,
-            expandedHeight: 120.0,
-            flexibleSpace: FlexibleSpaceBar(
-              titlePadding: EdgeInsetsDirectional.only(start: 15, bottom: 15),
-              title: Text('Welcome $userName'),
-              background: Container(
-                alignment: Alignment.centerRight,
-                width: size.width * 0.9,
-                child: UserIconButton(
-                  iconSize: 1.0,
-                  radius: 30,
-                  imageLink:
-                      "https://images.unsplash.com/photo-1491555103944-7c647fd857e6?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=300&q=80",
-                  color: Colors.white,
-                  margin: 20,
-                  push: () {
-                    context.read<PageCubit>().setPage(PageState.profilePage);
-                  },
-                ),
-              ),
-            )),
-        SliverList(
-          delegate: SliverChildBuilderDelegate(
-            (BuildContext context, int index) {
-              return BoardListElement(
-                board: boards[index],
-                boardLink: () {
-                  Navigator.pushNamed(context, boardDetailsRoute,
-                      arguments: boards[index]);
-                },
-                editLink: () {
-                  Navigator.pushNamed(context, editBoardPage,
-                      arguments: boards[index]);
-                },
-              );
+    return SliverList(
+      delegate: SliverChildBuilderDelegate(
+        (BuildContext context, int index) {
+          return BoardListElement(
+            board: boards[index],
+            boardLink: () {
+              Navigator.pushNamed(context, boardDetailsRoute,
+                  arguments: boards[index]);
             },
-            childCount: boards.length,
-          ),
-        ),
-        SliverList(
-          delegate: SliverChildListDelegate([
-            NewElementButton(
-              heightFraction: 0.15,
-              widthFraction: 0.8,
-              push: () {
-                Navigator.pushNamed(context, createBoardPage);
-              },
-            ),
-          ]),
-        ),
-      ],
+            editLink: () {
+              Navigator.pushNamed(context, editBoardPage,
+                  arguments: boards[index]);
+            },
+          );
+        },
+        childCount: boards.length,
+      ),
     );
   }
 }
