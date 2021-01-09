c2342e1cecf59ea12cc7c070a0ea9e3b362175fe
Gracjan Trawiński <44528307+gratraw@users.noreply.github.com>
2021-01-09 19:50:42 +0100

    Settings hotfix

--- lib/src/shared/components/board_list.dart
+++ lib/src/shared/components/board_list.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:uniq/src/blocs/page/page_cubit.dart';
 import 'package:uniq/src/models/board.dart';
 import 'package:uniq/src/shared/components/board_list_element.dart';
-import 'package:uniq/src/shared/components/new_element_button.dart';
-import 'package:uniq/src/shared/components/user_icon_button.dart';
 import 'package:uniq/src/shared/constants.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 class BoardList extends StatelessWidget {
   final List<Board> boards;
@@ -16,32 +12,18 @@ class BoardList extends StatelessWidget {
     return SliverList(
       delegate: SliverChildBuilderDelegate(
         (BuildContext context, int index) {
-          if (icon == Icon(Icons.settings)) {
-            return BoardListElement(
-              icon: icon,
-              board: boards[index],
-              boardLink: () {
-                Navigator.pushNamed(context, boardDetailsRoute,
-                    arguments: boards[index]);
-              },
-              iconAction: () {
-                Navigator.pushNamed(context, editBoardPage,
-                    arguments: boards[index]);
-              },
-            );
-          } else {
-            return BoardListElement(
-              icon: icon,
-              board: boards[index],
-              boardLink: () {
-                Navigator.pushNamed(context, boardDetailsRoute,
-                    arguments: boards[index]);
-              },
-              iconAction: () {
-                //TODO
-              },
-            );
-          }
+          return BoardListElement(
+            icon: icon,
+            board: boards[index],
+            boardLink: () {
+              Navigator.pushNamed(context, boardDetailsRoute,
+                  arguments: boards[index]);
+            },
+            iconAction: () {
+              Navigator.pushNamed(context, editBoardPage,
+                  arguments: boards[index]);
+            },
+          );
         },
         childCount: boards.length,
       ),
