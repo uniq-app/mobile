6509f403a2da9502c02f7a6f483df2b4cc1798b8
Gracjan Trawiński <44528307+gratraw@users.noreply.github.com>
2021-01-10 14:41:38 +0100

    I made mistake

--- lib/src/shared/components/board_list_element.dart
+++ lib/src/shared/components/board_list_element.dart
@@ -5,7 +5,7 @@ import 'package:uniq/src/services/photo_api_provider.dart';
 class BoardListElement extends StatelessWidget {
   final Board board;
   final VoidCallback boardLink, iconAction;
-  final double widthFraction, heightFraction;
+  final double widthFraction, heightFraction, iconSize;
   final Icon icon;
   BoardListElement(
       {Key key,
@@ -13,8 +13,9 @@ class BoardListElement extends StatelessWidget {
       this.boardLink,
       this.iconAction,
       this.widthFraction = 0.8,
-      this.heightFraction = 0.15,
-      this.icon})
+      this.heightFraction = 0.16,
+      this.icon,
+      this.iconSize = 26})
       : super(key: key);
 
   @override
@@ -41,49 +42,43 @@ class BoardListElement extends StatelessWidget {
             width: size.width * widthFraction,
             alignment: Alignment.topCenter,
             height: size.height * heightFraction,
-            child: Table(
-              //border: TableBorder.all(color: Colors.black),
-              columnWidths: {1: FractionColumnWidth(.1)},
+            child: Column(
               children: [
-                TableRow(children: [
-                  Container(
-                    padding: EdgeInsets.only(bottom: 3),
-                    child: Text(board.name,
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 36,
-                            fontWeight: FontWeight.w300)),
-                  ),
-                  IconButton(
-                    alignment: Alignment.topRight,
-                    padding: EdgeInsets.only(top: 0),
-                    icon: icon,
-                    color: Colors.white,
-                    iconSize: 28,
-                    onPressed: iconAction,
-                  )
-                ]),
-                TableRow(children: [
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(7),
-                    child: Container(
-                      color: Color(0x77181818),
-                      height: size.width * 0.11,
-                      child: Text(board.description ?? '',
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      padding: EdgeInsets.only(bottom: 3),
+                      child: Text(board.name,
                           style: TextStyle(
                               color: Colors.white,
-                              fontSize: 17,
-                              fontWeight: FontWeight.w200)),
+                              fontSize: 34,
+                              fontWeight: FontWeight.w300)),
                     ),
+                    IconButton(
+                      alignment: Alignment.topRight,
+                      padding: EdgeInsets.only(top: 0),
+                      icon: icon,
+                      color: Colors.white,
+                      iconSize: iconSize,
+                      onPressed: iconAction,
+                    ),
+                  ],
+                ),
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(7),
+                  child: Container(
+                    alignment: Alignment.topLeft,
+                    padding: EdgeInsets.symmetric(horizontal: 3),
+                    color: Color(0x77181818),
+                    height: size.height * heightFraction * 0.37,
+                    child: Text(board.description ?? '',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 17,
+                            fontWeight: FontWeight.w200)),
                   ),
-                  SizedBox()
-                ]),
-                TableRow(children: [
-                  SizedBox(
-                    height: size.height * 0.015,
-                  ),
-                  SizedBox()
-                ]),
+                ),
               ],
             ),
           ),
