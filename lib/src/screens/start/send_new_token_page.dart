57bf5fddb65f8c557d65e407d7853f6eedb384f1
Gracjan Trawiński <44528307+gratraw@users.noreply.github.com>
2021-01-15 14:08:22 +0100

    Completed form

--- lib/src/screens/start/send_new_token_page.dart
+++ lib/src/screens/start/send_new_token_page.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:oktoast/oktoast.dart';
 import 'package:uniq/src/blocs/user/user_bloc.dart';
+import 'package:uniq/src/shared/components/input_field.dart';
 import 'package:uniq/src/shared/components/loading.dart';
+import 'package:uniq/src/shared/components/uniq_button.dart';
 
 class SendNewTokenPage extends StatefulWidget {
   @override
@@ -22,14 +25,15 @@ class _SendNewTokenPageState extends State<SendNewTokenPage> {
     context.read<UserBloc>().add(ResendCode(email: emailController.text));
   }
 
+  final _emailKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return BlocListener<UserBloc, UserState>(
       listener: (context, UserState state) {
         if (state is ResendCodeSuccess) {
-          // TODO: Grachu check ,my inglisz
           showToast(
-            "Code has been send to your email",
+            "security code sent",
             position: ToastPosition.bottom,
             backgroundColor: Colors.green,
           );
@@ -43,23 +47,66 @@ class _SendNewTokenPageState extends State<SendNewTokenPage> {
         }
       },
       child: Scaffold(
-        body: Column(
-          children: [
-            Center(
-              child: Text("We no dziabnij forma"),
+        body: Container(
+          padding: EdgeInsets.symmetric(horizontal: size.width * 0.05),
+          child: Center(
+            child: Form(
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+              key: _emailKey,
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      height: size.height * 0.35,
+                      child: SvgPicture.asset("assets/images/completed.svg"),
+                    ),
+                    SizedBox(height: size.height * 0.02),
+                    Text(
+                      "resend security code",
+                      style: Theme.of(context).textTheme.headline3,
+                    ),
+                    SizedBox(height: size.height * 0.03),
+                    UniqInputIconField(
+                      color: Theme.of(context).accentColor,
+                      inputIcon: Icons.email,
+                      isObscure: false,
+                      labelText: "email",
+                      controller: emailController,
+                      validator: (value) {
+                        if (value.isEmpty) return 'Please enter the email';
+                        if (!value.contains("@") || !value.contains("."))
+                          return 'Please enter correct email';
+                        return null;
+                      },
+                    ),
+                    SizedBox(height: size.height * 0.01),
+                    Text(
+                      "we will send you security code for authorization",
+                      style: Theme.of(context).textTheme.caption,
+                    ),
+                    SizedBox(height: size.height * 0.03),
+                    BlocBuilder<UserBloc, UserState>(
+                      builder: (context, UserState state) {
+                        if (state is ResendCodeLoading) {
+                          return Loading();
+                        }
+                        return UniqButton(
+                          color: Theme.of(context).buttonColor,
+                          push: () {
+                            if (_emailKey.currentState.validate()) {
+                              _sendNewCode;
+                            }
+                          },
+                          text: "send security code",
+                        );
+                      },
+                    ),
+                  ],
+                ),
+              ),
             ),
-            BlocBuilder<UserBloc, UserState>(
-              builder: (context, UserState state) {
-                if (state is ResendCodeLoading) {
-                  return Loading();
-                }
-                return OutlineButton(
-                  onPressed: _sendNewCode,
-                  child: Text('Send code'),
-                );
-              },
-            ),
-          ],
+          ),
         ),
       ),
     );
